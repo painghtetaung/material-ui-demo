8d7488009e72e61d07a76f46610025d628b302ff
painghtetaung <painghtetaung1999@gmail.com>
2021-12-01 17:02:15 +0630

    typography

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import {Typography} from '@material-ui/core'
 
 const App = () => {
   return (
     <div>
-      <h1>Hello, World</h1>
+      <Typography variant="h1">Hello, World</Typography>
     </div>
   );
 }
